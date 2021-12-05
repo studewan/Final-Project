d973fcf3e3dbdebde3d0002d2e8954c8a3c747c9
studewan <89421563+studewan@users.noreply.github.com>
2021-12-04 20:19:26 -0500

    commit

--- Enemy/Enemies.gd
+++ Enemy/Enemies.gd
@@ -3,6 +3,6 @@ extends Node2D
 var Enemy = preload("res://Enemy/Enemy.tscn")
 
 func _physics_process(_delta):
-	if get_child_count() < Global.level:
+	if get_child_count() == 0:
 		var e = Enemy.instance()
 		add_child(e)
